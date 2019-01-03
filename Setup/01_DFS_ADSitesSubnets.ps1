aa8c4a56625d73125c3f36289f99042215bc3fb9
Cornelius Schuchardt <cornelius.schuchardt@gmail.com>
2019-01-03 11:18:55 +0100

    first sync added: Setup Subnets

---
+++ Setup/01_DFS_ADSitesSubnets.ps1
@@ -0,0 +1,18 @@
+﻿# Rename Default Site
+Get-ADObject -SearchBase (Get-ADRootDSE).ConfigurationNamingContext -filter "objectclass -eq 'site'" | Rename-ADObject -NewName Default -Confirm:$false
+
+# Add Default Subnet
+New-ADReplicationSubnet -Site Default -Name 192.168.0.0/24
+
+# Add Site NewYork
+New-ADReplicationSite NewYork
+
+# Add Site LosAngeles
+New-ADReplicationSite LosAngeles
+
+# Add Single IP Subnets
+New-ADReplicationSubnet -Site NewYork -Name 192.168.0.251/32
+New-ADReplicationSubnet -Site NewYork -Name 192.168.0.19/32
+New-ADReplicationSubnet -Site LosAngeles -Name 192.168.0.87/32
+New-ADReplicationSubnet -Site LosAngeles -Name 192.168.0.44/32
+New-ADReplicationSubnet -Site LosAngeles -Name 192.168.0.68/32
