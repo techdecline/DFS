e8aec27cc59d65a6f66784d529e17dd9e8bec8e2
Schuchardt <schuchardt@softed.de>
2019-01-03 19:48:22 +0100

    Added Scripts for first module

---
+++ Labs/Lab01_01_Standalone_DFS_Namespace.ps1
@@ -0,0 +1,4 @@
+﻿# LAB 1 Standalone DFS
+
+# Install Windows Feature "DFS Namespaces"
+Install-WindowsFeature -Name FS-DFS-Namespace -IncludeManagementTools -Restart
---
+++ Setup/02_DHCP_Reservation.ps1
@@ -0,0 +1,17 @@
+﻿$addressHash = @{
+    "DFS-FS-LA" = "192.168.0.87"
+    "DFS-FS-VA" = "192.168.0.44"
+    "DFS-FS-MX" = "192.168.0.68"
+    "DFS-FS-NY" = "192.168.0.19"
+    "DFS-CL-NY" = "192.168.0.251"
+}
+
+$leaseArr = Get-DhcpServerv4Lease -ScopeId 192.168.0.0
+
+
+foreach ($lease in $leaseArr) {
+    $hostName = ($lease.hostName -split '\.')[0]
+    Add-DhcpServerv4Reservation -ScopeId 192.168.0.0 -IPAddress $addressHash.Item($hostName) `
+        -ClientId $lease.ClientId -Name $hostName
+    Invoke-Command -ScriptBlock {ipconfig /release; ipconfig /renew} -ComputerName $hostName
+}
