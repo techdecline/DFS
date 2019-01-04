aeffca666b99ce4d33d30311d0162a5801129681
Cornelius Schuchardt <cornelius.schuchardt@gmail.com>
2019-01-04 13:20:56 +0100

    Lab3 checkin

---
+++ Labs/Lab03_01_Standalone_DFS_Namespace.ps1
@@ -0,0 +1,7 @@
+﻿# Lab 03 Add Referral Server
+
+# Install DFS Namespace feature on DFS-FS-LA
+Install-WindowsFeature FS-DFS-Namespace -ComputerName DFS-FS-LA
+
+# Add secondary server as DFS Referral Server
+New-DfsnRootTarget -Path \\dfs.lab\Software -TargetPath \\DFS-FS-LA\Software
