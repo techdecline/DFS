589cfe5c11fd76fab38effdfead64e076da600ce
Cornelius Schuchardt <cornelius.schuchardt@gmail.com>
2019-01-04 09:48:59 +0100

    Update Lab02

---
+++ Labs/Lab02_01_Standalone_DFS_Namespace.ps1
@@ -0,0 +1,15 @@
+﻿# LAB 2 Domain DFS
+
+# Install Windows Feature "DFS Namespaces"
+Install-WindowsFeature -Name FS-DFS-Namespace -IncludeManagementTools -Restart
+
+# Create New Namespace 
+New-DfsnRoot -Type DomainV2 -Path \\dfs.lab\Software -TargetPath \\dfs-fs-ny\Software
+
+# Add local Example Folders
+"Adobe","Microsoft","Google" | ForEach-Object {
+    New-DfsnFolder -Path (Join-Path \\dfs.lab\Software -ChildPath $_) -TargetPath "\\DFS-FS-NY\$_"
+}
+
+# Add remote example folder
+New-DfsnFolder -Path \\dfs.lab\Software\Apple -TargetPath \\DFS-FS-LA\Apple
